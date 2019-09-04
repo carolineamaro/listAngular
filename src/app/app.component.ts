20902083c0a697f3d948f9f3d48449d36f1aae50
Caroline Correa <caroline.amaro@hotmail.com>
2019-09-04 00:07:54 -0300

    Adicionando Projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  todoArray = [] 
+  
+  addTodo(value) {
+    this.todoArray.push(value);
+    //console.log(this.todos)
+  }
+  deleteTodo(value) {  
+    for(let i=0 ;i<= this.todoArray.length ;i++) {    
+      if(value == this.todoArray[i]) {     
+        this.todoArray.splice(i,1)    
+      }   
+    }  
+  }
+}
